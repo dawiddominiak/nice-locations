11d898d3aa847b851323b9e40a31f6578adebcf4
dawiddominiak <poczta@dawiddominiak.pl>
2019-04-08 01:39:30 +0200

    LocationService unit test is passing

--- src/location/location.service.spec.ts
+++ src/location/location.service.spec.ts
@@ -1,18 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { LocationService } from './location.service';
+import { Repository } from 'typeorm';
+import { Location } from './location.entity';
+import * as sinon from 'sinon';
 
 describe('LocationService', () => {
   let service: LocationService;
+  let mockRepository: Repository<Location>;
+  let mock: sinon.SinonMock;
 
   beforeEach(async () => {
-    service = new LocationService();
+    mockRepository = { } as Repository<Location>;
+    mockRepository = Object.assign(mockRepository, {
+      find: new Function(),
+    });
+    mock = sinon.mock(mockRepository);
+    mock.expects('find').resolves([
+
+    ]);
+
+    service = new LocationService(mockRepository);
   });
 
   it('should return locations', () => {
-    expect(service.list()).toMatchObject([
-      'Location 1',
-      'Location 2',
-      'Location 3',
+    expect(service.list()).resolves.toMatchObject([
     ]);
   });
 });
