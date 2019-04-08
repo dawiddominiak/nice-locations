32c47b9f65afabceed50b0e2bc4a027014d0d7ae
dawiddominiak <poczta@dawiddominiak.pl>
2019-04-08 02:07:37 +0200

    Adding new locations to location service test

--- src/location/location.service.spec.ts
+++ src/location/location.service.spec.ts
@@ -3,6 +3,7 @@ import { LocationService } from './location.service';
 import { Repository } from 'typeorm';
 import { Location } from './location.entity';
 import * as sinon from 'sinon';
+import { GenericFactory } from '../common/generic.factory';
 
 describe('LocationService', () => {
   let service: LocationService;
@@ -16,7 +17,14 @@ describe('LocationService', () => {
     });
     mock = sinon.mock(mockRepository);
     mock.expects('find').resolves([
-
+      GenericFactory.create<Location>(Location, {
+        id: '4a7ba206-2376-4ed4-87ac-40e8a2df9341',
+        name: 'Location 1',
+      }),
+      GenericFactory.create<Location>(Location, {
+        id: 'f4000db9-3496-4b42-8bf0-2c336109ab78',
+        name: 'Location 2',
+      }),
     ]);
 
     service = new LocationService(mockRepository);
@@ -24,6 +32,14 @@ describe('LocationService', () => {
 
   it('should return locations', () => {
     expect(service.list()).resolves.toMatchObject([
+      {
+        id: '4a7ba206-2376-4ed4-87ac-40e8a2df9341',
+        name: 'Location 1',
+      },
+      {
+        id: 'f4000db9-3496-4b42-8bf0-2c336109ab78',
+        name: 'Location 2',
+      },
     ]);
   });
 });
